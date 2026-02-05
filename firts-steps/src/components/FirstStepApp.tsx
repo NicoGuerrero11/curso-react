48b83c5aad02d53ae4e15735ad06e37d818720c9
NicolasGuerrero <nicoguerrero0800@gmail.com>
2026-02-04 21:13:39 -0600

    agregamos items de forma mas dinamica

--- firts-steps/src/components/FirstStepApp.tsx
+++ firts-steps/src/components/FirstStepApp.tsx
@@ -1,10 +1,30 @@
+import { ItemCounter } from "../shopping-cart/ItemCounter";
+
+interface Item {
+    productName: string,
+    quantity: number
+}
+
+const initialItems: Item[] = [
+    { productName: 'Nintendo Switch 2', quantity: 10 },
+    { productName: 'PlayStation 5', quantity: 15 },
+    { productName: 'Xbox One', quantity: 20 },
+]
 
 export function FirstStepApp() {
   return (
     <>
-      <h1>Hola Mundo</h1>
-      <p>esto es un parrafo</p>
-      <h2> esto es un h2</h2>
+      <h1>Carrito de compras</h1>
+      {
+        initialItems.map(({productName, quantity}) => (
+          <ItemCounter name={productName} quantity={quantity}/>
+        ))
+      }
+
+      {/* <ItemCounter name="Nintendo Switch 2" quantity={5}/>
+      <ItemCounter name="PlayStation 5" quantity={10}/>
+      <ItemCounter name="Xbox One" quantity={20}/> */}
+      
     </>
   )
 }
