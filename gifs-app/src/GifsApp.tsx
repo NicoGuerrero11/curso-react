e0be64937cdd6249c2bd94b14e3769622977b238
NicolasGuerrero <nicoguerrero0800@gmail.com>
2026-02-07 10:55:46 -0600

    feat: implement GIF search functionality and update previous searches handling

--- gifs-app/src/GifsApp.tsx
+++ gifs-app/src/GifsApp.tsx
@@ -4,15 +4,30 @@ import { CustomHeader } from './components/CustomHeader';
 import { PreviousSearches } from './components/PreviousSearches';
 import { SearchBar } from './components/SearchBar';
 import { GifsList } from './components/GifsList';
+import { getGifsByQuery } from './gifs/get-gifs-by-query.action';
+import type { Gif } from './interfaces/gif.interface';
+
+
+
 export const GifsApp = () => {
-    const [previousTerm, setPreviousTerm] = useState(["Goku"])
+
+    const [gifs, setGifs] = useState<Gif[]>([])
+    const [previousTerm, setPreviousTerm] = useState<string[]>([])
 
     const handlerClicked = (term:string) => {
         console.log(term)
     }
-    const handlerSearch = (query:string) => {
-        console.log(query);
-    }
+    const handlerSearch = async (query:string) => {
+        const term = query.trim().toLocaleLowerCase()
+        if (!term)return;
+        if(previousTerm.includes(term))return;
+        setPreviousTerm((prev) => {
+            const updated = [term, ...prev];
+            return updated.slice(0, 8);
+        });
+        const gifs = await getGifsByQuery(query)
+        setGifs(gifs)
+    };
 
     return (
         <>
@@ -28,7 +43,11 @@ export const GifsApp = () => {
             <PreviousSearches title='Busquedas Previas' list={previousTerm} onLabelClick={handlerClicked}/>
 
             {/* gifs */}
-            <GifsList gifs={mockGifs}/>
+            {gifs.length > 0 ? (
+                <GifsList gifs={gifs} />
+            ) : (
+                <GifsList gifs={mockGifs} />
+            )}
         </>
     )
 }
