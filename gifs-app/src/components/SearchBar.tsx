bdce145ca352d180c3123762f14217c98e2410f4
NicolasGuerrero <nicoguerrero0800@gmail.com>
2026-02-07 10:55:57 -0600

    refactor: improve formatting and structure of SearchBar component

--- gifs-app/src/components/SearchBar.tsx
+++ gifs-app/src/components/SearchBar.tsx
@@ -1,35 +1,44 @@
-import {useState } from "react"
+import { useState, useEffect } from "react"
 
-interface Props{
-    placeholder? : string,
-    buttonTitle : string
-    onQuery: (query:string) => void
+interface Props {
+  placeholder?: string,
+  buttonTitle: string
+  onQuery: (query: string) => void
 }
 
-export const SearchBar = ({placeholder = "Buscar", buttonTitle, onQuery}: Props) => {
+export const SearchBar = ({ placeholder = "Buscar", buttonTitle, onQuery }: Props) => {
 
-const [query, setquery] = useState('')
+  const [query, setquery] = useState('')
 
-const handlerSearch= () => {
-  onQuery(query)
-  setquery('')
-}
-const handleKeyDown = (event:React.KeyboardEvent<HTMLInputElement> ) => {
-  if (event.key === 'Enter'){
-              handlerSearch()
-            }
-}
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onQuery(query);
+    }, 700)
+    return () => {
+      clearTimeout(timeoutId);
+    }
+  }, [query, onQuery])
+
+  const handlerSearch = () => {
+    onQuery(query)
+    setquery('')
+  }
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handlerSearch()
+    }
+  }
 
   return (
     <div className="search-container">
-        <input  
-          type="text" 
-          placeholder={placeholder} 
-          value={query}
-          onChange={(event) => setquery(event.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        <button onClick={handlerSearch}>{buttonTitle}</button>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={query}
+        onChange={(event) => setquery(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handlerSearch}>{buttonTitle}</button>
     </div>
   )
 }
